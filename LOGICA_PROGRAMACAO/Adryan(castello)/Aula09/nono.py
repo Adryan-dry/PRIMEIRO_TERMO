77f9632b9c51e520e384d42be212aed4ad42f288
Adryan Costa <adryangalzerano00@gmail.com>
2026-05-13 11:28:55 -0300

    Atualizando

--- LOGICA_PROGRAMACAO/Adryan(castello)/Aula09/nono.py
+++ LOGICA_PROGRAMACAO/Adryan(castello)/Aula09/nono.py
@@ -1,2 +1,78 @@
-print("Bem-vindo ao GItHub")#aula 9 13/05/2026
-#ainda vai começar
+#LISTA DE ATIVIDADES 2 - TA
+#ERRADO
+# 1. O Problema da Idade
+# idade = input("Digite sua idade: ")
+# if idade >= 18:
+# print("Você é maior de idade.")
+
+# # CORRIGIDO:
+# 1.O Problema da idade:
+# idade = int(input("Digite sua idade"))
+# if idade >= 18:
+#     print("Você é maior de idade ")
+
+# else: 
+#     print("Você é menor de idade")
+
+
+# # MELHORADO:
+# idade = int(input("Qual sua idade?"))
+# if idade < 18 :
+#     print("Peça ajuda para alguém de maior idade:)")
+
+
+# 2. A Escrita Fiel
+# nome = "Mariana"
+# print("Seja bem-vinda, nome!")    
+
+
+# CORRIGIDO:
+# nome = "Mariana"
+# print("Seja bem-vinda" , nome)
+
+# #MELHORADO:
+# print(f"Seja bem-vinda, {nome}!")
+
+
+# 3. Falta de Espaço
+#ERRADO:
+# numero = 10
+# if numero > 5:
+# print("O número é maior que cinco.")
+# else:
+# print("O número é menor ou igual a cinco.")
+
+#CORRIGIDO:
+
+# numero = 10
+# if numero > 5:
+#     print("O número é maior que cinco.")
+# else:
+#      print("O número é menor ou igual a cinco.")
+
+#FAZER O MELHORADO AINDA 
+
+# 4. Esquecimento Fatal
+# usuario = "aluno123"
+# if usuario == "aluno123"
+# print("Login realizado com sucesso.")
+
+
+
+
+
+
+# 5. Atribuição vs. Comparação
+# clima = "ensolarado"
+# if clima = "chuvoso":
+# print("Leve um guarda-chuva!")
+
+
+
+
+
+
+
+# 6. Misturando Alhos com Bugalhos
+# pontos = 50
+# print("Parabéns! Você fez " + pontos + " pontos.")
